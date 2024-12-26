b5f6bd353dd2d2932bf7d6a6ba6603ec0ad2669b
Maged Eshak <magedeshak39@gmail.com>
2024-12-26 13:44:15 +0200

    ex 2

--- ObjectsAndMethods.js
+++ ObjectsAndMethods.js
@@ -35,7 +35,43 @@ console.log("-".repeat(20));
 console.log("3- Nested Object And Advanced Examples");
 console.log("-".repeat(20));
 
+let person = {
+  // properties
+  theName: "Maged",
+  theAge: 25,
+  skills: ["HTML", "CSS", "JS"],
+  availability: false,
 
+  // Nested Object
+  country: {
+    ksa: "Riyadh",
+      // Nested Object
+    egypt: {
+      one: "Assuit",
+      two: "Cairo",
+    },
+  },
+// function to check if person Free to Hire or not
+  CheckAV: function () {
+    if (person.availability === true) {
+      return `Free to Hire`;
+    } else {
+      return `Not Free to Hire`;
+    }
+  }
+};
+console.log(person.theName); // Dot notation
+console.log(person.theAge); 
+console.log(person.skills); // Array
+console.log(person.skills.join(" | ")); // HTML | CSS | JS
+console.log(person.skills.push("React")); // item number
+console.log(person.skills[3]); // index 3 in array = React
+console.log(person.availability); 
+console.log(person.country); 
+console.log(person.country.ksa); // Riyadh
+console.log(person.country.egypt.one); // Assuit
+console.log(person["country"]["egypt"]["two"]); // Bracket notation => cairo
+console.log(person.CheckAV()); // checking availability
 
 console.log("-".repeat(20));
 console.log("4- Create Object With New Keyword");
