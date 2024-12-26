d3ee92b18cc12f6ef9487b51913f2a1c374f4288
Maged Eshak <magedeshak39@gmail.com>
2024-12-26 13:19:32 +0200

    update

--- ObjectsAndMethods.js
+++ ObjectsAndMethods.js
@@ -1,15 +1,40 @@
 console.log("1- Object – Introduction");
 console.log("-".repeat(20));
 
+let user = {
+  // properties
+  theName: "Maged",
+  theAge: 25,
+
+  //Methods
+  sayHello : function () {
+    return `Hello`;
+  },
+};
+console.log(user.theName);
+console.log(user.theAge);
+console.log(user.sayHello());
 
 console.log("-".repeat(20));
 console.log("2- Dot Notation vs Bracket Notation");
 console.log("-".repeat(20));
 
+let myVar = "country";
+
+let use = {
+  // properties
+  theName: "Maged",
+  country: "Egypt",
+  "age": 25,
+};
+console.log(use.theName); // Dot notation
+console.log(use["age"]); // Bracket notation
+console.log(use[myVar]); // Bracket notation
 
 console.log("-".repeat(20));
 console.log("3- Nested Object And Advanced Examples");
 console.log("-".repeat(20));
+
 
 
 console.log("-".repeat(20));
