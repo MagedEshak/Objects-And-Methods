fb448308316d07e6f274bf360b468dbeb88aeb3c
Maged Eshak <magedeshak39@gmail.com>
2024-12-26 12:38:47 +0200

    first commit

---
+++ ObjectsAndMethods.js
@@ -0,0 +1,33 @@
+console.log("1- Object – Introduction");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("2- Dot Notation vs Bracket Notation");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("3- Nested Object And Advanced Examples");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("4- Create Object With New Keyword");
+console.log("-".repeat(20));
+
+
+
+console.log("-".repeat(20));
+console.log("5- This Keyword");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("6- Create Object With Create Method");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("7- Create Object With Assign Method");
+console.log("-".repeat(20));
